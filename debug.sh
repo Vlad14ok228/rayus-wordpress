b1d622e4ed8d261d550df1b9894132a6ed87ba40
Vlad14ok228 <vlad14ok228@github.com>
2026-04-06 16:59:58 +0300

    Restructure: organize files into folders, remove redundant files

--- debug.sh
+++
@@ -1,2 +0,0 @@
-﻿#!/bin/bash
-docker exec wordpress sed -i 's/define( .WP_DEBUG., false );/define( "WP_DEBUG", true );\ndefine( "WP_DEBUG_LOG", true );\ndefine( "WP_DEBUG_DISPLAY", true );/' /var/www/html/wp-config.php
